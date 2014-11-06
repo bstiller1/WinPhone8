ce9b7ebbd7d54d36c12015213ee59045460551a4
bstiller1 <stiller.blake@gmail.com>
2014-11-06 17:00:26 -0500

    Windows Phone 8
    epochCount App with epoch time displayed in a textbox.

---
+++ epochCount/epochCount/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using epochCount.Resources;
+
+namespace epochCount
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ epochCount/epochCount/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using epochCount.Resources;
+
+namespace epochCount
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            DateTime unixEpoch = new DateTime(1970, 1, 1);
+            DateTime currentDate = DateTime.Now;
+            long totalMiliSecond = (currentDate.Ticks - unixEpoch.Ticks) / 10000;
+            String totalMillis = Convert.ToString(totalMiliSecond);
+            cdTimer.Text = totalMillis;
+        }
+
+       
+    }
+}
